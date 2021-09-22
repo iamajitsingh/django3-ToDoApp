825672b360dc4a1532cf911386c49d5779859827
iamajitsingh <iamajit41@gmail.com>
2021-09-22 10:54:16 +0530

    Refactored the html pages, updated footer

--- the_wall/local_settings.py
+++
@@ -1,5 +0,0 @@
-DEBUG = True
-ALLOWED_HOSTS = []
-SECRET_KEY = 'kz#il#2qfwf!lq#=i=so$#+0+ds3(c*+mvdn)$kg&!&2+y4d=s'
-EMAIL_HOST_USER = 'wallofproject@gmail.com'
-EMAIL_HOST_PASSWORD = 'jesuisiamajit'
