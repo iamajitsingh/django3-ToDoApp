d2db352d843b4ea76955f5132f331cbdeb9a0a90
iamajitsingh <iamajit41@gmail.com>
2021-09-22 04:58:06 +0000

    Updated settings

--- the_wall/settings.py
+++ the_wall/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY",default='ThisCanBeEasilyHacked')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", default=True)
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [os.getenv("HOST", default='')]
 
@@ -120,13 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #HOST SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", default='')
-EMAIL_HOST_PASSWORD = ("EMAIL_HOST_PASSWORD", default='')
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", default='')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
